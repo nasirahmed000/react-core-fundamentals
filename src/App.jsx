c53837738b4c9deffbee452b2c768a5adb31371b
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-10-24 22:07:37 +0600

    more core concept code added

--- src/App.jsx
+++ src/App.jsx
@@ -95,41 +95,275 @@
 //     />
 //   ); 
 // }
-const people = [{
-  id: 0,
-  name: 'Creola Katherine Johnson',
-  profession: 'mathematician',
-}, {
-  id: 1,
-  name: 'Mario José Molina-Pasquel Henríquez',
-  profession: 'chemist',
-}, {
-  id: 2,
-  name: 'Mohammad Abdus Salam',
-  profession: 'physicist',
-}, {
-  id: 3,
-  name: 'Percy Lavon Julian',
-  profession: 'chemist',  
-}, {
-  id: 4,
-  name: 'Subrahmanyan Chandrasekhar',
-  profession: 'astrophysicist',
-}];
+// const people = [{
+//   id: 0,
+//   name: 'Creola Katherine Johnson',
+//   profession: 'mathematician',
+// }, {
+//   id: 1,
+//   name: 'Mario José Molina-Pasquel Henríquez',
+//   profession: 'chemist',
+// }, {
+//   id: 2,
+//   name: 'Mohammad Abdus Salam',
+//   profession: 'physicist',
+// }, {
+//   id: 3,
+//   name: 'Percy Lavon Julian',
+//   profession: 'chemist',  
+// }, {
+//   id: 4,
+//   name: 'Subrahmanyan Chandrasekhar',
+//   profession: 'astrophysicist',
+// }];
+
+//  export default function App(){
+//   const chemists = people.filter(person => person.profession === 'chemist');
+
+//   const listItems = chemists.map((chemists)=> <li key={chemists.id}>{chemists.name}</li>)
 
 
- export default function App(){
-  const chemists = people.filter(person => person.profession === 'chemist');
+//      return (
+//        <div>
+//         <div>
+//           {listItems} 
+//         </div>
+//        </div> 
+//      );
+//  }
 
-  const listItems = chemists.map((chemists)=> <li key={chemists.id}>{chemists.name}</li>)
+//   export default function App() {
+//     function handleClick (){
+//       console.log("button clicked")
+//     }
+//   return (
+//     <button onClick={handleClick}> I don't do anything  </button>
+//   );
+// }
+
+   
+// function AlertButton({ message, children }) {
+//   return (
+//     <button onClick={() => alert(message)}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <AlertButton message="Playing!">
+//         Play Movie
+//       </AlertButton>
+//       <AlertButton message="Uploading!">
+//         Upload Image
+//       </AlertButton>
+//     </div>
+//   );
+// }
+
+// function Button({ onSmash, children }) {
+//   return (
+//     <button onClick={onSmash}>
+//       {children}
+//     </button>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div>
+//       <Button onSmash={() => alert('Playing!')}>
+//         Play Movie
+//       </Button>
+//       <Button onSmash={() => alert('Uploading!')}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <Toolbar
+//       onPlayMovie={() => alert('Playing!')}
+//       onUploadImage={() => alert('Uploading!')}
+//     />
+//   );  
+// }
+
+// function Toolbar({ onPlayMovie, onUploadImage }) {
+//   return (
+//     <div>
+//       <Button onClick={onPlayMovie}>
+//         Play Movie
+//       </Button>
+//       <Button onClick={onUploadImage}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={onClick}>
+//       {children}
+//     </button>
+//   );
+// }
 
 
-     return (
-       <div>
-        <div>
-          {listItems}
-        </div>
-       </div>
-     );
- }
+// export default function Toolbar() {
+//   return (
+//     <div className="Toolbar" onClick={() => {
+//       alert('You clicked on the toolbar!');
+//     }}>
+//       <button onClick={() => alert('Playing!')}>
+//         Play Movie
+//       </button>
+//       <button onClick={() => alert('Uploading!')}>
+//         Upload Image
+//       </button>
+//     </div>
+//   );
+// }
+
+
+
+// function Button({ onClick, children }) {
+//   return (
+//     <button onClick={e => {
+//       e.stopPropagation();
+//       onClick();
+//     }}>
+//       {children}
+//     </button>
+//   );
+// }
+ 
+
+// export default function Toolbar() {
+//   return (
+//     <div className="Toolbar" onClick={() => {
+//       alert('You clicked on the toolbar!');
+//     }}>
+//       <Button onClick={() => alert('Playing!')}>
+//         Play Movie
+//       </Button>
+//       <Button onClick={() => alert('Uploading!')}>
+//         Upload Image
+//       </Button>
+//     </div>
+//   );
+// }
+ 
+// export default function Signup() {
+//   return (
+//     <form onSubmit={(e) => alert('Submitting!')}>
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+// export default function Signup() {
+//   return (
+//     <form onSubmit={e => {
+//       e.preventDefault();
+//       alert('Submitting!');
+//     }}>
+//       <input />
+//       <button>Send</button>
+//     </form>
+//   );
+// }
+
+
+// import { sculptureList } from './data.js';
+
+// import { useState } from 'react';
+
+
+
+// export default function Gallery() {
+
+//  const [index, setIndex] = useState(0);
+
+
+//   function handleClick() { 
+//     setIndex (index +1);
+//   }
+
+
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleClick}>
+//         Next
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         by {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} of {sculptureList.length})
+//       </h3>
+//       <img   
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//       <p>
+//         {sculpture.description}
+//       </p>
+//     </>
+//   );
+// }
+
+
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
+
+
+
+
 
