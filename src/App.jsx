a4a4c5c4f1837144440b3065912d4c27a12f63f2
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-10-25 19:29:22 +0600

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -323,45 +323,139 @@
 // }
 
 
-import { useState } from 'react';
-import { sculptureList } from './data.js';
+// import { useState } from 'react';
+// import { sculptureList } from './data.js';
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
+// export default function Gallery() {
+//   const [index, setIndex] = useState(0);
+//   const [showMore, setShowMore] = useState(false);
 
-  function handleNextClick() {
-    setIndex(index + 1);
-  }
+//   function handleNextClick() {
+//     setIndex(index + 1);
+//   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
-  }
+//   function handleMoreClick() {
+//     setShowMore(!showMore);
+//   }
 
-  let sculpture = sculptureList[index];
-  return (
-    <>
-      <button onClick={handleNextClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i> 
-        by {sculpture.artist}
-      </h2>
-      <h3>  
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-    </>
-  );
-} 
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//       <button onClick={handleNextClick}>
+//         Next
+//       </button>
+//       <h2>
+//         <i>{sculpture.name} </i> 
+//         by {sculpture.artist}
+//       </h2>
+//       <h3>  
+//         ({index + 1} of {sculptureList.length})
+//       </h3>
+//       <button onClick={handleMoreClick}>
+//         {showMore ? 'Hide' : 'Show'} details
+//       </button>
+//       {showMore && <p>{sculpture.description}</p>}
+//       <img 
+//         src={sculpture.url} 
+//         alt={sculpture.alt}
+//       />
+//     </>
+//   );
+// } 
 
+// import { useState } from 'react';
 
+// export default function MovingDot() {
+//   const [position, setPosition] = useState({
+//     x: 0,
+//     y: 0
+//   });
+//   return (
+//     <div
+//       onPointerMove={e => {
+//         setPosition({
+//           x: e.clientX,
+//           y: e.clientY
+//         });
+//       }}
+//       style={{
+//         position: 'relative',
+//         width: '100vw',
+//         height: '100vh',
+//       }}>
+//       <div style={{
+//         position: 'absolute',
+//         backgroundColor: 'red',
+//         borderRadius: '50%',
+//         transform: `translate(${position.x}px, ${position.y}px)`,
+//         left: -10,
+//         top: -10,
+//         width: 20,
+//         height: 20,
+//       }} />
+//     </div>
+//   );
+// }
+
+// import { useState } from 'react';
+// export default function Form() {
+
+//   const [person, setPerson] = useState({
+//     firstName: 'Barbara',
+//     lastName: 'Hepworth',
+//     email: 'bhepworth@sculpture.com'
+//   });
+
+//   function handleFirstNameChange(e) {
+//     setPerson({
+//       ...person,
+//       firstName: e.target.value
+//     });
+//   }
+
+//   function handleLastNameChange(e) {
+//     setPerson({
+//       ...person,
+//       lastName: e.target.value
+//     });
+//   }
+
+//   function handleEmailChange(e) {
+//     setPerson({
+//       ...person,
+//       email: e.target.value
+//     });
+//   }
+
+//   return (
+//     <>
+//       <label>
+//         First name:
+//         <input
+//           value={person.firstName}
+//           onChange={handleFirstNameChange}
+//         />
+//       </label>
+//       <label>
+//         Last name:
+//         <input
+//           value={person.lastName}
+//           onChange={handleLastNameChange}
+//         />
+//       </label>
+//       <label>
+//         Email:
+//         <input
+//           value={person.email}
+//           onChange={handleEmailChange}
+//         />
+//       </label>
+//       <p>
+//         {person.firstName}{' '}
+//         {person.lastName}{' '}
+//         ({person.email})
+//       </p>
+//     </>
+//   ); 
+// }
 
