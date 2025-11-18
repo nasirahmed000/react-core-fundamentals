652d27bbde2709f0a75113be83e83a576de790e9
Nasir Ahmed <ahsannasirahmed@gmail.com>
2025-11-18 16:43:28 +0600

    preservinf state between cmponencts

--- src/App.jsx
+++ src/App.jsx
@@ -908,6 +908,166 @@
 // }
 
 // export default App;
+// ==========================================================================================================
+// // import { useState } from "react";
+// // export default function InputExample() {
+// //   const [text, setText] = useState("");
+// //   const handleChange = (e) => {
+// //     setText(e.target.value);
+// //   };
+// //   return (
+// //     <div style={{ textAlign: "center", marginTop: "30px" }}>
+// //       <input
+// //         type="text"
+// //         placeholder="Type something..."
+// //         value={text}
+// //         onChange={handleChange}
+// //         style={{ padding: "8px", fontSize: "16px" }}
+// //       />
+// //       <p>You typed: {text}</p>
+// //     </div>
+// //   );
+// // }
+
+// // import TravelPlan from "./components/TravelPlan";
+
+// // function App() {
+// //     return <TravelPlan />;
+// // }
+
+// // export default App;
+
+// // sharing between components
+
+// //  import { useState } from 'react';
+
+// // function Panel({ title, children }) {
+// //   const [isActive, setIsActive] = useState(false);
+// //   return (
+// //     <section className="panel">
+// //       <h3>{title}</h3>
+// //       {isActive ? (
+// //         <p>{children}</p>
+// //       ) : (
+// //         <button onClick={() => setIsActive(true)}>
+// //           Show
+// //         </button>
+// //       )}
+// //     </section>
+// //   );
+// // }
+
+// // export default function Accordion() {
+// //   return (
+// //     <>
+// //       <h2>Almaty, Kazakhstan</h2>
+// //       <Panel title="About">
+// //         With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+// //       </Panel>
+// //       <Panel title="Etymology">
+// //         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+// //       </Panel>
+// //     </>
+// //   );
+// // }
+
+// // lifting state up example
+// // ==================================================================================================
+
+// import { useState } from 'react';
+
+// export default function Accordion() {
+//   const [activeIndex, setActiveIndex] = useState(0);
+//   return (
+//     <>
+//       <h2>Almaty, Kazakhstan</h2>
+//       <Panel
+//         title="About"
+//         isActive={activeIndex === 0}
+//         onShow={() => setActiveIndex(0)}
+//       >
+//         With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+//       </Panel>
+//       <Panel
+//         title="Etymology"
+//         isActive={activeIndex === 1}
+//         onShow={() => setActiveIndex(1)}
+//       >
+//         The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
+//       </Panel>
+//     </>
+//   );
+// }
+
+// function Panel({
+//   title,
+//   children,
+//   isActive,
+//   onShow
+// }) {
+//   return (
+//     <section className="panel">
+//       <h3>{title}</h3>
+//       {isActive ? (
+//         <p>{children}</p>
+//       ) : (
+//         <button onClick={onShow}>
+//           Show
+//         </button>
+//       )}
+//     </section>
+//   );
+// }
+// // ==================================================================================================
+
+// import { useState } from 'react';
+
+// export default function App() {
+//   const [isPaused, setIsPaused] = useState(false);
+//   return (
+//     <div>
+//       {isPaused ? (
+//         <p>See you later!</p>
+//       ) : (
+//         <Counter />
+//       )}
+//       <label>
+//         <input
+//           type="checkbox"
+//           checked={isPaused}
+//           onChange={e => {
+//             setIsPaused(e.target.checked)
+//           }}
+//         />
+//         Take a break
+//       </label>
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   const [score, setScore] = useState(0);
+//   const [hover, setHover] = useState(false);
+
+//   let className = 'counter';
+//   if (hover) {
+//     className += ' hover';
+//   }
+
+//   return (
+//     <div
+//       className={className}
+//       onPointerEnter={() => setHover(true)}
+//       onPointerLeave={() => setHover(false)}
+//     >
+//       <h1>{score}</h1>
+//       <button onClick={() => setScore(score + 1)}>
+//         Add one
+//       </button>
+//     </div>
+//   );
+// }
+// ==================================================================================================
 
 import { useState } from "react";
 
@@ -942,64 +1102,4 @@ export default function NameInputExample() {
     </div>
   );
 }
-
-// import { useState } from "react";
-// export default function InputExample() {
-//   const [text, setText] = useState("");
-//   const handleChange = (e) => {
-//     setText(e.target.value);
-//   };
-//   return (
-//     <div style={{ textAlign: "center", marginTop: "30px" }}>
-//       <input
-//         type="text"
-//         placeholder="Type something..."
-//         value={text}
-//         onChange={handleChange}
-//         style={{ padding: "8px", fontSize: "16px" }}
-//       />
-//       <p>You typed: {text}</p>
-//     </div>
-//   );
-// }
-
-// import TravelPlan from "./components/TravelPlan";
-
-// function App() {
-//     return <TravelPlan />;
-// }
-
-// export default App;
-
-
- import { useState } from 'react';
-
-function Panel({ title, children }) {
-  const [isActive, setIsActive] = useState(false);
-  return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? (
-        <p>{children}</p>
-      ) : (
-        <button onClick={() => setIsActive(true)}>
-          Show
-        </button>
-      )}
-    </section>
-  );
-}
-
-export default function Accordion() {
-  return (
-    <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel title="About">
-        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel title="Etymology">
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-      </Panel>
-    </>
-  );
-}
+// ==================================================================================================
